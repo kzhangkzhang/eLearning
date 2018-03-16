0410f59fc40bf313b7416173ab05f3e65fd2fa7a
Kevin Zhang <kevin.zhang@knowles.com>
2018-03-16 11:02:25 -0500

    Delete drop_moviefact_hivepartb.sh

--- BigData/R/OracleLab/OLH_OSCH/example_scripts/osch/tuning/drop_moviefact_hivepartb.sh
+++
@@ -1,9 +0,0 @@
-# Add HIVE_HOME/lib* to HADOOP_CLASSPATH.  This is not preset 
-# in the VM since this breaks Pig in the previous lab.
-
-export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$HIVE_HOME/lib/*
-
-hadoop jar $OSCH_HOME/jlib/orahdfs.jar \
-       oracle.hadoop.exttab.ExternalTable \
-       -conf /home/oracle/movie/moviework/osch/tuning/moviefact_hivepartb.xml \
-       -drop
